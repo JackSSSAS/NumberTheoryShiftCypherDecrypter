6f0bfcf233fd1a721d5f384fd106846401459390
JackSSSAS <74074379+JackSSSAS@users.noreply.github.com>
2024-04-23 18:24:43 -0400

    Update Main.java
    Add homework ciphers to main

--- Main.java
+++ Main.java
@@ -1,6 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        Message d = new Message("BNPOH VT"); // Input string here!
+        Message d;
+        
+        d = new Message("FUNXRVGBSS"); // Input string here!
+        d.decryptInput(); // Call the decryptInput method on the Message Object
+
+        d = new Message("RCBJLADNUBDVVNA"); // Input string here!
+        d.decryptInput(); // Call the decryptInput method on the Message Object
+
+        d = new Message("WQPQIJHAWRAOBWHHEJC"); // Input string here!
+        d.decryptInput(); // Call the decryptInput method on the Message Object
+
+        d = new Message("BJSJAJWLTTZYTKXYDQJ"); // Input string here!
         d.decryptInput(); // Call the decryptInput method on the Message Object
     }
 }
